0784979e1fdb5462fd2a50ac845cd82cc6fd5afd
rajan <rajangreedge@gmail.com>
2019-03-03 12:08:57 +0530

    craeted componetn folder and App.js

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div className="App">
+            App
+        </div>
+    );
+ };
+
+ export default App;
