92487d33a7573373fc0a994d1c5bc3ca886cc088
rajan <rajangreedge@gmail.com>
2019-03-03 12:13:50 +0530

    created SearchBar

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import SearchBar from './SearchBar'
 
 const App = () => {
     return (
         <div className="App">
-            App
+            <SearchBar />
         </div>
     );
  };
