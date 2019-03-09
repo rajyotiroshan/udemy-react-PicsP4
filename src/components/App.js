6c015884f9244060ef2f52098d3d7963e4c0f550
rajan <rajangreedge@gmail.com>
2019-03-09 20:31:53 +0530

    fixed this keyword value inside onSearchSubmit method

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,9 @@ import SearchBar from './SearchBar'
 
 class App extends React.Component {
 
-  async onSearchSubmit(term){
+    state = {images: []};
+
+   onSearchSubmit = async (term)=>{
         //call to unsplash api.
        const response = await axios.get('https://api.unsplash.com/search/photos',{
             params: {query: term},
@@ -13,14 +15,15 @@ class App extends React.Component {
                 Authorization: 'Client-ID 940166052fabfd53a01780b3fd3363db5b431f84e31906f6f57fc0cbda806552'
             }
         });
-
-        console.log(response.data.results);
+        //console.log(this);
+       this.setState({images: response.data.results});
     }
 
     render(){
         return (
         <div className="ui container"style={{marginTop: '10px'}}>
             <SearchBar onSubmit = {this.onSearchSubmit}/>
+           Found::{this.state.images.length} images.
         </div>
     );
  }
