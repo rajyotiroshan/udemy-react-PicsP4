5c4644c930b3b55143620f0e27b09431d70ba968
rajan <rajangreedge@gmail.com>
2019-03-09 17:03:47 +0530

    fixed error

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar'
 
-class App extends React.Component() {
+class App extends React.Component {
     render(){
         return (
         <div className="App">
